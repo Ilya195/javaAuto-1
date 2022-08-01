e31e02dd7dabd0f456c6bb60614a2fc548ef8aa5
Ilya195 <vodoley_agent@inbox.ru>
2022-08-01 15:16:56 +0600

    Changed src files

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,6 +4,11 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        return boundary - amount % boundary;
+        if (amount < boundary) {
+            return boundary - amount % boundary;
+        }
+        if (amount >= boundary) {
+        }
+        return amount;
     }
 }
--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,11 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.TestNG;
+import static org.testng.Assert.*;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
 
 public class CashbackHackServiceTest {
 
+    @Test
     public void shouldRemain() {
         CashbackHackService service = new CashbackHackService();
 
@@ -14,7 +13,17 @@ public class CashbackHackServiceTest {
         int expected = 650;
         int actual = service.remain(amount);
 
+        assertEquals( actual,  expected);
+    }
 
-        //void assertEquals(int actual, int expected);
+    @Test
+    public void shouldHighRemain() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 3000;
+        int expected = 3000;
+        int actual = service.remain(amount);
+
+        assertEquals( actual,  expected);
     }
 }
