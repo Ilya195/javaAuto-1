32f67b8923881a78f68e307f5a0fe54c6e49b802
Ilya195 <vodoley_agent@inbox.ru>
2022-08-07 21:43:36 +0600

    Changed test file

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,11 +4,11 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        if (amount < boundary) {
+      //  if (amount < boundary) {
             return boundary - amount % boundary;
-        }
-        if (amount >= boundary) {
-        }
-        return amount;
+      //  }
+      //  if (amount >= boundary) {
+      //  }
+      //  return amount;
     }
 }
